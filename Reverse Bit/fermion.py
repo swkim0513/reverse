d292b6ff3a4c0b70accb3ad18c52563c53c3ec4c
swk <dkx0513@naver.com>
2016-12-16 06:29:39 +0900

    mp3 test

--- Reverse Bit/fermion.py
+++ Reverse Bit/fermion.py
@@ -3,6 +3,9 @@ from pico2d import*
 class Fermion():
     def __init__(self):
         self.image = load_image('fermion.png')
+        self.bgm = load_music('test.mp3')
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
     def draw(self):
         self.image.clip_draw(0,0,1200,1100,600,550)
     def update(self):
